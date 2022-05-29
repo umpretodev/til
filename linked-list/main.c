4cb889951108acd18592d40dc7658b7420cfdbf4
Pedro Fernandes <dr.pedrofernades@gmail.com>
2022-05-29 17:32:42 -0300

    list: métodos fundamentais na lista de produtos

--- linked-list/main.c
+++ linked-list/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node {
-    char name[30];
+    char* name;
     double price;
     int token; 
 
@@ -17,13 +18,61 @@ typedef struct list {
 } List;
 
 
-void pushNode() {};
+//add pointer recursive
+void pushNode( List* list, char* name, double price, int token) {
+    if(!list->head) {
+        list->head = (Node *)malloc(sizeof(Node));
+        list->tail = (Node *)malloc(sizeof(Node));
+        
+        list->head->name = name;
+        list->head->price = price;
+        list->head->token = token;
 
-void searchNode( int token) {};
+        list->tail = list->head;
+    }
 
-void __list__( List* list) {};
+    else {
+        list->tail->next = (Node *)malloc(sizeof(Node));
+        list->tail = list->tail->next;
+        
+        list->tail->name = name;
+        list->tail->price = price;
+        list->tail->token = token;
+    }
+}
+
+//search pointer recursive
+void searchNode(Node* pointer, int token) {
+    if(pointer) {
+        if(pointer->token == token) {
+            printf("product: %s",pointer->name);
+        }
+
+        else searchNode(pointer->next, token);
+    }
+}
+
+//show list
+void __printf__( Node* pointer) {
+    if(pointer) {
+        printf("token: %d -> ", pointer->token);
+        __printf__(pointer->next);
+    }
+
+    else printf("/\n");
+}
+
 
 int main() {
+    List list; 
+
+    pushNode(&list, "arroz", 5.00, 1);
+    pushNode(&list, "feijão", 5.00, 2);
+    pushNode(&list, "feijão", 5.00, 3);
+    pushNode(&list, "feijão", 5.00, 4);
+    
+    __printf__(list.head);
+
 
 
     return 0;
